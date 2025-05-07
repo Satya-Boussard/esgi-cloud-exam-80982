e4bb8f4fa9acea336c9eacd874ca96c46362feb3
Boussard Satya <sboussardard2@myges.fr>
2025-05-07 15:07:31 +0200

    bdd

--- db.client.js
+++ db.client.js
@@ -1,19 +1,22 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 
 // database
+const bdd = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  bdd, 
   {
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-  },
+    logging: false,
+  }
 );
 
-// authentication and synchronization
+// authentication and synchronisation
 sequelize.authenticate()
   .then(() => {
     sequelize.sync().catch(() => console.log("Cannot sync the database"));
